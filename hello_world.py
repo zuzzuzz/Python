8c7557debaf441a2e7a12d16ca78ff996c15a13d
Duy Lyford <61067182+zuzzuzz@users.noreply.github.com>
2023-03-10 09:35:38 -0600

    python string practice

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+# 1. TASK: print "Hello World"
+greetings = "Hello World"
+print(greetings)
+
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Duy"
+print("Hello", name)	# with a comma
+print("Hello " + name)	# with a +
+
+# 3. print "Hello 42!" with the number in a variable
+num = 8
+print("Hello", num)	# with a comma
+print("hello" + " " + str(num))	# with a +-- this one should give us an error!
+
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "pad see eww"
+fave_food2 = "pho"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2)) 
+print(f"I love to eat {fave_food1} and {fave_food2}.")
+
