3633666c9d4e439d8871d32c11e322a31a3da3fe
Duy Lyford <61067182+zuzzuzz@users.noreply.github.com>
2023-03-10 09:43:37 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,13 +4,13 @@ print(greetings)
 
 # 2. print "Hello Noelle!" with the name in a variable
 name = "Duy"
-print("Hello", name)	# with a comma
-print("Hello " + name)	# with a +
+print("Hello", name,"!")	# with a comma
+print("Hello " + name + "!")	# with a +
 
 # 3. print "Hello 42!" with the number in a variable
-num = 8
-print("Hello", num)	# with a comma
-print("hello" + " " + str(num))	# with a +-- this one should give us an error!
+name = 8
+print("Hello", name, "!")	# with a comma
+print("hello" + str(8) +"!")	# with a +-- this one should give us an error!
 
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "pad see eww"
